d29f510cd2109422e7b7286f5fb576a30d8e8c58
prof <prof@Pracownia12>
2017-10-13 12:12:50 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ cw3/cw3.js
@@ -0,0 +1,14 @@
+
+window.onload = start;
+
+function start(){
+    var pars = document.getElementsByTagName("p");
+   // alert(pars[pars.length-1].innerText);
+   // alert(pars[pars.length-1].innerHTML);
+  // pars[1].innerHTML = "Tego jeszcze nie było";
+  var tekst = prompt("Podaj nową treść: ");
+  pars[2].innerHTML = tekst;
+  pars[3].style.color = "red";
+  pars[3].style.fontSize = "2em";
+}
+

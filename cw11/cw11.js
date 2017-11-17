00c0b3aa092488ddb6525f6fae500179232dab0f
prof <prof@Pracownia12>
2017-11-17 12:15:45 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw11/cw11.js
+++ cw11/cw11.js
@@ -1,30 +1,39 @@
 window.onload = start;
 
-function start(){
+function start() {
     var dane = [
-        ["Polska","Warszawa"],
-        ["Rosja","Moskwa"],
-        ["Niemcy","Berlin"],
-        ["Francja","Paryż"],
-        ["USA","Waszyngton"]
+        ["Polska", "Warszawa"],
+        ["Rosja", "Moskwa"],
+        ["Niemcy", "Berlin"],
+        ["Francja", "Paryż"],
+        ["USA", "Waszyngton"],
+        ["Wielka Brytania", "Londyn"],
+        ["Holandia", "Amsterdam"]
     ];
     var wylosowany = GenerPanstwo();
     document.getElementById("panstwo").innerHTML = dane[wylosowany][0];
     document.getElementById("wynik").innerHTML = GenerSelect();
     document.getElementById("sprawdz").onclick = klik;
-    function klik(){
-        alert('klik');
+    function klik() {
+        var wybor = document.getElementById("stolica").value;
+        if (wybor == wylosowany) {
+            document.getElementById("rezultat")
+                    .innerHTML = "Prawidłowa odpowiedź";
+        } else {
+            document.getElementById("rezultat")
+                    .innerHTML = "Coś nie za bardzo z geografią";
+        }
     }
-    function GenerPanstwo(){
-        var losowa = Math.floor(Math.random()*dane.length);
+    function GenerPanstwo() {
+        var losowa = Math.floor(Math.random() * dane.length);
         console.log(losowa);
         return losowa;
-    } 
-    function GenerSelect(){
+    }
+    function GenerSelect() {
         var html = "<select id='stolica'>";
-        for(var i=0;i<dane.length;i++){
-            html += "<option value='"+i+"'>"+dane[i][1]+"</option>";
+        for (var i = 0; i < dane.length; i++) {
+            html += "<option value='" + i + "'>" + dane[i][1] + "</option>";
         }
-        return html+"</select>";
+        return html + "</select>";
     }
 }

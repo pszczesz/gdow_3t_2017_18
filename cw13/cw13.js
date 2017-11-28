70bfd293eb681bd2641820ce4f4ad01b1e0aea93
prof <prof@192.168.0.9>
2017-11-28 12:26:14 +0100

    ff2

--- cw13/cw13.js
+++ cw13/cw13.js
@@ -5,7 +5,24 @@ var images = ["motyl0.png","motyl1.png","motyl2.png","motyl3.png",
                "motyl4.png","motyl5.png","motyl6.png","motyl7.png",
                "motyl8.png","motyl9.png"];
 function start(){
-  setInterval(losuj,2000);
+  setInterval(losuj,5000);
+  document.getElementById("up").onclick = goUp;
+  document.getElementById("down").onclick = goDown;
+}
+function goUp(){
+    actual = actual+1;
+    if(actual>images.length-1) actual = 0;
+    document.getElementById("obrazek")
+             .setAttribute("src","imgs/"+images[actual]);
+    document.getElementById("podpis").innerHTML = images[actual];
+}
+function goDown(){
+    actual = actual-1;
+    console.log(actual);
+    if(actual<0) actual = images.length-1;
+    document.getElementById("obrazek")
+             .setAttribute("src","imgs/"+images[actual]);
+    document.getElementById("podpis").innerHTML = images[actual];
 }
 function losuj(){
     actual = Math.floor(Math.random()*images.length);

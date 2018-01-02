59f22568f62ac74419c6333bfa99dc34a2b70137
prof <prof@192.168.0.9>
2018-01-02 12:09:29 +0100

    ff2

---
+++ cw18/cw18.js
@@ -0,0 +1,38 @@
+window.onload = start;
+function start(){
+    document.getElementById("graj").onclick = gener;
+}
+function gener(){
+    var rowsOK = Validate(document.getElementById("rows"));
+    var colsOK = Validate(document.getElementById("cols"));
+    if(rowsOK && colsOK){
+        var rows = parseInt(document.getElementById("rows").value);
+        var cols = parseInt(document.getElementById("cols").value);
+        document.getElementById("wynik").innerHTML = tabelka(rows,cols);
+        //document.getElementById("wynik").innerHTML = "Tabelka";
+    }else{
+        document.getElementById("wynik").innerHTML = "Brak tabelki";
+    }
+}
+function tabelka(rows,cols){
+    var html = "<table>";
+    for(var i=0;i<rows;i++){
+        html += "<tr>";
+        for(var j=0;j<cols;j++){
+            html += "<td></td>";
+        }
+        html += "</tr>";
+    }
+    return html+"</table>";
+}
+function Validate(elem){
+    var value = parseInt(elem.value);
+    var next = elem.nextElementSibling;
+    if(!isNaN(value) && value>2 && value<=50){
+        next.innerHTML = "";
+        return true;
+    }else{
+        next.innerHTML = "Błedne dane lub poza zakresem między 3 i 50!";
+        return false;
+    }
+}

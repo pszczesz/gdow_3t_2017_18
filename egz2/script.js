e7b1dc7596f5c797dc305246a9cdb7262c7b07af
prof <prof@Pracownia12>
2018-04-24 11:17:54 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- egz2/script.js
+++ egz2/script.js
@@ -1,27 +1,22 @@
-window.onload = function (){
-    document.getElementById("oblicz").onclick = function (){
+window.onload = function () {
+    document.getElementById("oblicz").onclick = function () {
         var wyb = document.getElementById("news").checked;
         var ilosc = parseInt(document.getElementById("ilosc").value);
-        if(isNaN(ilosc)){ 
-            document.getElementById("wynik").innerHTML = "BRAK DANYCH";
-            return;
+        var wynik = "";
+        if (!isNaN(ilosc) && ilosc > 0) {//           
+            var bonus = wyb ? 0.2 : 0;
+            
+            if (ilosc > 0 && ilosc <= 10) {
+               wynik = (1 * ilosc - (bonus * ilosc)).toFixed(2);
+            } else if (ilosc <= 50) {
+                wynik = (0.8 * ilosc - (bonus * ilosc)).toFixed(2);
+            } else {
+                wynik = (0.8 * ilosc - (bonus * ilosc)).toFixed(2);
+            }
+        } else {
+            wynik = "BRAK DANYCH";            
         }
-        var bonus = wyb ? 0.2 : 0;
-        if (ilosc<=0){
-            document.getElementById("wynik").innerHTML = "BRAK DANYCH";
-            return;
-        }
-        if(ilosc>0 && ilosc<=10){
-           document.getElementById("wynik").innerHTML = 
-                   (1* ilosc- (bonus*ilosc)).toFixed(2); 
-        }else if(ilosc<=50){
-           document.getElementById("wynik").innerHTML = 
-                   (0.8* ilosc-(bonus*ilosc)).toFixed(2); 
-        }else {
-            document.getElementById("wynik").innerHTML = 
-                    (0.8* ilosc-(bonus*ilosc)).toFixed(2);
-        }
-        
+        document.getElementById("wynik").innerHTML = wynik;
     };
 };
 
